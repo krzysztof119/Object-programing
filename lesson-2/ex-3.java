1e36a7d68ff3e035c0b91646badeac70bf7c7ab5
krzysztof119 <67243080+krzysztof119@users.noreply.github.com>
2022-11-05 13:00:20 +0100

    Update ex-3.java

--- lesson-2/ex-3.java
+++ lesson-2/ex-3.java
@@ -8,40 +8,45 @@ class Author {
         this.lastName = lastName;
         this.age = age;
     }
+    
+    public String display(){
+        return (this.name + " " + this.lastName + " " + this.age);
+    }
 }
 
 class Game {
     private String title;
-    private String author;
+    private Author author;
     private float rating;
     private float size;
     
-    public Game(String title, String author, float rating, float size){
+    public Game(String title, Author author, float rating, float size){
         this.title = title;
         this.author = author;
         this.rating = rating;
         this.size = size;
     }
-    public Game(String title, String author, double rating, float size){
+    public Game(String title, Author author, double rating, float size){
         this(title,  author, (float) rating, size);
     }
     
-    public Game(String title, String author, float rating, double size){
+    public Game(String title, Author author, float rating, double size){
         this(title,  author, rating, (float) size);
     }
     
-    public Game(String title, String author, double rating, double size){
+    public Game(String title, Author author, double rating, double size){
         this(title,  author, (float) rating, (float) size);
     }
     
     public void display(){
-        System.out.println("Game title: " + title  + ",\nCreated by: " + author + ",\nRated for: " + rating + ",\nRequired disk space: " + size);
+        System.out.println("Game title: " + title  + ",\nCreated by: " + author.display() + ",\nRated for: " + rating + ",\nRequired disk space: " + size);
     }
 }
 
 class main {
     public static void main(String[] args) {
-        Game Game1 = new Game("BD", "Janusz Kowalski", 3.5 , 1.3);
-        Game1.display();
+        Author author1 = new Author("Janusz", "Kowalski", 23);
+        Game game1 = new Game("BD", author1, 3.5 , 1.3);
+        game1.display();
     }
 }
