1deda6510583223ddb7db101fd876fb2850f2933
krzysztof119 <67243080+krzysztof119@users.noreply.github.com>
2022-12-10 14:50:14 +0100

    Create ex-7.java

---
+++ lesson-3/ex-7.java
@@ -0,0 +1,35 @@
+interface Number{
+    double doubleValue();
+}
+
+class Logarithm implements Number {
+  private double base, argument;
+        @Override
+        public double doubleValue() { 
+            try{
+                if(base<=0){
+                    throw new ArithmeticException("error: base " +  Double.toString(base) + " cannot be less then 0\n");
+                }
+                return Math.log(this.argument) / Math.log(this.base);
+            }   catch(ArithmeticException x){
+                throw new IllegalArgumentException(
+                    String.format(
+                        "Cannot make calculations for this number: %d",
+                        x
+                    ),
+                x
+                );
+            }
+        }
+  public Logarithm(double inputBase, double inputArgument) {
+    this.base = inputBase;
+    this.argument = inputArgument;
+  }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Logarithm Log = new Logarithm(1, 0);
+        System.out.println(Log.doubleValue());
+    }
+}
