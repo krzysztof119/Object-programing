ed675787a847e0e9e1b6e5c62dee08988308c89b
krzysztof119 <67243080+krzysztof119@users.noreply.github.com>
2022-12-15 00:26:52 +0100

    Update main.java

--- project/main.java
+++ project/main.java
@@ -1,44 +1,50 @@
 import java.util.*;
-interface X{
-    
+
+interface IBook{
+    boolean rent(Member X);
 }
 
 class Library{
     private String name;
-    private String lastName;
     
-    public Library(String name, String lastName){
+    public Library(String name){
         this.name=name;
-        this.lastName=lastName;
+        
     }
     
     public void displayName(){
         System.out.print(this.name+"\n");
     }
-    public void displayLastName(){
-        System.out.print(this.lastName+"\n");
-    }
+
 }
 
-class Member extends Library{
+class Member{
+    private String lastName;
+    private String name;
     private int borrowed_limit = 5;
     private Vector<String> borrowed_books = new Vector<String>(borrowed_limit);
     private int debt = 0;
     
-    public Member(String name, String lastname){
-        super(name, lastname);
+    public Member(String name, String lastname){   
+        this.name=name;
+        this.lastName=lastName;
     }
     
-    public void addrent(String... args){
+    public boolean AddRent(String... args){
         if(args.length > borrowed_limit-borrowed_books.size()){
             System.out.print("Request canceled due to exceeding the limit of rental books possible"+"\n");
-            return;
+            return false;
         }
         borrowed_books.addAll(Arrays.asList(args));
         System.out.print(borrowed_books+" "+borrowed_books.size()+"\n");
+        return true;
     }
 
-    public void displayDebt(){
+    public void DisplayLastName(){
+        System.out.print(this.lastName+"\n");
+    }
+
+    public void DisplayDebt(){
         System.out.print(this.debt+"\n");
     }
 }
@@ -46,8 +52,8 @@ class Member extends Library{
 public class Main {
     public static void main(String[] args) {
         Member czytelnik1 = new Member("Pawel", "Kowalski");
-        czytelnik1.addrent("0", "1", "2", "3", "4", "5");czytelnik1.addrent("0");
+        czytelnik1.AddRent("0", "1", "2", "3", "4", "5");czytelnik1.AddRent("0");
 
-        czytelnik1.displayName();czytelnik1.displayLastName();czytelnik1.displayDebt();
+        czytelnik1.DisplayLastName();czytelnik1.DisplayDebt();
     }
 }
