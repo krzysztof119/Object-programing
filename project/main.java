3ffd5ad3596b3e290c2a6d1e1b739bc89202ca72
krzysztof119 <67243080+krzysztof119@users.noreply.github.com>
2022-12-14 18:33:40 +0100

    Create main.java

---
+++ project/main.java
@@ -0,0 +1,40 @@
+interface X{
+    
+}
+
+class Library{
+    private String name;
+    private String lastName;
+    
+    public Library(String name, String lastName){
+        this.name=name;
+        this.lastName=lastName;
+    }
+    
+    public void displayName(){
+        System.out.println(this.name+"\n");
+    }
+    public void displayLastName(){
+        System.out.println(this.lastName+"\n");
+    }
+}
+
+class Member extends Library{
+    private String borrowed_books = "";
+    private int debt = 0;
+    
+    public Member(String name, String lastname){
+        super(name, lastname);
+    }
+    
+    public void displayDebt(){
+        System.out.println(this.debt+"\n");
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Member czytelnik1 = new Member("Pawel", "Kowalski");
+        czytelnik1.displayName();czytelnik1.displayLastName();czytelnik1.displayDebt();
+    }
+}
