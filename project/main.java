8069a56f238de7852a15c3f2096c100235a58e7c
krzysztof119 <67243080+krzysztof119@users.noreply.github.com>
2022-12-14 22:46:04 +0100

    Update main.java

--- project/main.java
+++ project/main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 interface X{
     
 }
@@ -12,29 +13,44 @@ class Library{
     }
     
     public void displayName(){
-        System.out.println(this.name+"\n");
+        System.out.print(this.name+"\n");
     }
     public void displayLastName(){
-        System.out.println(this.lastName+"\n");
+        System.out.print(this.lastName+"\n");
     }
 }
 
 class Member extends Library{
-    private String borrowed_books = "";
+    private int borrowed_limit = 5;
+    private Vector<String> borrowed_books = new Vector<String>(borrowed_limit);
     private int debt = 0;
     
     public Member(String name, String lastname){
         super(name, lastname);
     }
     
+    public void addrent(String... args){
+        if(args.length > borrowed_limit-borrowed_books.size()){
+            System.out.print("Request canceled due to exceeding the limit of rental books possible"+"\n");
+            return;
+        }
+        for(String arg : args){
+            borrowed_books.add(arg);
+            borrowed_limit += 1;
+        }
+        System.out.print(borrowed_books+" "+borrowed_books.size()+"\n");
+    }
+
     public void displayDebt(){
-        System.out.println(this.debt+"\n");
+        System.out.print(this.debt+"\n");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Member czytelnik1 = new Member("Pawel", "Kowalski");
+        czytelnik1.addrent("Nic", "Test", "1", "2", "3", "4");czytelnik1.addrent();
+
         czytelnik1.displayName();czytelnik1.displayLastName();czytelnik1.displayDebt();
     }
 }
